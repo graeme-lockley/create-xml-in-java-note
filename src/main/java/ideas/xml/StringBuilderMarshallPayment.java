0858ae5e222e811c09d90421ab3b14ba2c634e1d
graemel <graemel@no9.co.za>
2015-12-30 11:35:19 +0200

    Renamed the parameter paymentValueValue to paymentValue.

--- src/main/java/ideas/xml/StringBuilderMarshallPayment.java
+++ src/main/java/ideas/xml/StringBuilderMarshallPayment.java
@@ -4,75 +4,75 @@ import static ideas.xml.dsl.XMLEncoder.encode;
 
 public class StringBuilderMarshallPayment implements MarshallPayment {
     @Override
-    public String marshall(PaymentValue paymentValueValue) {
+    public String marshall(PaymentValue paymentValue) {
         StringBuilder marshallResult = new StringBuilder();
 
         marshallResult
                 .append("<payment>")
                 .append("<creator application-name=")
                 .append("\"")
-                .append(paymentValueValue.creatorApplicationName)
+                .append(paymentValue.creatorApplicationName)
                 .append("\"")
                 .append(" client-id=")
                 .append("\"")
-                .append(paymentValueValue.creatorClientID)
+                .append(paymentValue.creatorClientID)
                 .append("\"")
                 .append(" correlation-id=")
                 .append("\"")
-                .append(paymentValueValue.correlationID)
+                .append(paymentValue.correlationID)
                 .append("\"")
                 .append("/>")
-                .append("<reference>").append(paymentValueValue.reference).append("</reference>")
-                .append("<when").append(" year=").append("\"").append(paymentValueValue.transactionDate.substring(0, 4)).append("\"")
-                .append(" month=").append("\"").append(paymentValueValue.transactionDate.substring(4, 6)).append("\"")
-                .append(" day=").append("\"").append(paymentValueValue.transactionDate.substring(6, 8)).append("\"")
-                .append(" hour=").append("\"").append(paymentValueValue.transactionTime.substring(0, 2)).append("\"")
-                .append(" minute=").append("\"").append(paymentValueValue.transactionTime.substring(2, 4)).append("\"")
-                .append(" second=").append("\"").append(paymentValueValue.transactionTime.substring(4, 6)).append("\"")
+                .append("<reference>").append(paymentValue.reference).append("</reference>")
+                .append("<when").append(" year=").append("\"").append(paymentValue.transactionDate.substring(0, 4)).append("\"")
+                .append(" month=").append("\"").append(paymentValue.transactionDate.substring(4, 6)).append("\"")
+                .append(" day=").append("\"").append(paymentValue.transactionDate.substring(6, 8)).append("\"")
+                .append(" hour=").append("\"").append(paymentValue.transactionTime.substring(0, 2)).append("\"")
+                .append(" minute=").append("\"").append(paymentValue.transactionTime.substring(2, 4)).append("\"")
+                .append(" second=").append("\"").append(paymentValue.transactionTime.substring(4, 6)).append("\"")
                 .append("/>")
-                .append("<action-date").append(" year=").append("\"").append(paymentValueValue.valueDate.substring(0, 4)).append("\"")
-                .append(" month=").append("\"").append(paymentValueValue.valueDate.substring(4, 6)).append("\"")
-                .append(" day=").append("\"").append(paymentValueValue.valueDate.substring(6, 8)).append("\"")
+                .append("<action-date").append(" year=").append("\"").append(paymentValue.valueDate.substring(0, 4)).append("\"")
+                .append(" month=").append("\"").append(paymentValue.valueDate.substring(4, 6)).append("\"")
+                .append(" day=").append("\"").append(paymentValue.valueDate.substring(6, 8)).append("\"")
                 .append("/>")
-                .append("<currency>").append(paymentValueValue.currency).append("</currency>")
-                .append("<amount>").append(paymentValueValue.amount).append("</amount>")
-                .append("<debit-leg").append(" application-name=").append("\"").append(paymentValueValue.debitApplication).append("\"");
-        if (paymentValueValue.debitSortCode != null) {
-            marshallResult.append(" sort-code=").append("\"").append(paymentValueValue.debitSortCode).append("\"");
+                .append("<currency>").append(paymentValue.currency).append("</currency>")
+                .append("<amount>").append(paymentValue.amount).append("</amount>")
+                .append("<debit-leg").append(" application-name=").append("\"").append(paymentValue.debitApplication).append("\"");
+        if (paymentValue.debitSortCode != null) {
+            marshallResult.append(" sort-code=").append("\"").append(paymentValue.debitSortCode).append("\"");
         }
-        if (paymentValueValue.debitAccountNumber != null) {
-            marshallResult.append(" account=").append("\"").append(paymentValueValue.debitAccountNumber).append("\"");
+        if (paymentValue.debitAccountNumber != null) {
+            marshallResult.append(" account=").append("\"").append(paymentValue.debitAccountNumber).append("\"");
         }
-        if (paymentValueValue.debitAccountName != null) {
-            marshallResult.append(" name=").append("\"").append(encode(paymentValueValue.debitAccountName)).append("\"");
+        if (paymentValue.debitAccountName != null) {
+            marshallResult.append(" name=").append("\"").append(encode(paymentValue.debitAccountName)).append("\"");
         }
-        if (paymentValueValue.debitPaymentReference != null) {
-            marshallResult.append(" reference=").append("\"").append(encode(paymentValueValue.debitPaymentReference)).append("\"");
+        if (paymentValue.debitPaymentReference != null) {
+            marshallResult.append(" reference=").append("\"").append(encode(paymentValue.debitPaymentReference)).append("\"");
         }
-        if (paymentValueValue.debitLedgerAccountNumber != null) {
-            marshallResult.append(" ledger-account-number=").append("\"").append(paymentValueValue.debitLedgerAccountNumber).append("\"");
+        if (paymentValue.debitLedgerAccountNumber != null) {
+            marshallResult.append(" ledger-account-number=").append("\"").append(paymentValue.debitLedgerAccountNumber).append("\"");
         }
         marshallResult
                 .append("/>")
                 .append("<credit-leg")
                 .append(" application-name=")
                 .append("\"")
-                .append(paymentValueValue.creditApplication)
+                .append(paymentValue.creditApplication)
                 .append("\"");
-        if (paymentValueValue.creditSortCode != null) {
-            marshallResult.append(" sort-code=").append("\"").append(paymentValueValue.creditSortCode).append("\"");
+        if (paymentValue.creditSortCode != null) {
+            marshallResult.append(" sort-code=").append("\"").append(paymentValue.creditSortCode).append("\"");
         }
-        if (paymentValueValue.creditAccountNumber != null) {
-            marshallResult.append(" account=").append("\"").append(paymentValueValue.creditAccountNumber).append("\"");
+        if (paymentValue.creditAccountNumber != null) {
+            marshallResult.append(" account=").append("\"").append(paymentValue.creditAccountNumber).append("\"");
         }
-        if (paymentValueValue.creditAccountName != null) {
-            marshallResult.append(" name=").append("\"").append(encode(paymentValueValue.creditAccountName)).append("\"");
+        if (paymentValue.creditAccountName != null) {
+            marshallResult.append(" name=").append("\"").append(encode(paymentValue.creditAccountName)).append("\"");
         }
-        if (paymentValueValue.creditPaymentReference != null) {
-            marshallResult.append(" reference=").append("\"").append(encode(paymentValueValue.creditPaymentReference)).append("\"");
+        if (paymentValue.creditPaymentReference != null) {
+            marshallResult.append(" reference=").append("\"").append(encode(paymentValue.creditPaymentReference)).append("\"");
         }
-        if (paymentValueValue.creditLedgeAccountNumber != null) {
-            marshallResult.append(" ledger-account-number=").append("\"").append(paymentValueValue.creditLedgeAccountNumber).append("\"");
+        if (paymentValue.creditLedgeAccountNumber != null) {
+            marshallResult.append(" ledger-account-number=").append("\"").append(paymentValue.creditLedgeAccountNumber).append("\"");
         }
         marshallResult
                 .append("/>")
