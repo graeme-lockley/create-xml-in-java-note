979688bb1817996f07fa34fb3865420de178bf81
graemel <graemel@no9.co.za>
2016-01-16 10:49:21 +0200

    Renamed paymentValueValue to paymentValue.

--- src/main/java/ideas/xml/JAXBMarshallPayment.java
+++ src/main/java/ideas/xml/JAXBMarshallPayment.java
@@ -31,48 +31,48 @@ public abstract class JAXBMarshallPayment implements MarshallPayment {
 
     public abstract Marshaller getMarshaller();
 
-    protected Payment getPayment(PaymentValue paymentValueValue) {
+    protected Payment getPayment(PaymentValue paymentValue) {
         generated.CreatorType createApplication = new generated.CreatorType();
-        createApplication.setApplicationName(paymentValueValue.creatorApplicationName);
-        createApplication.setClientId(paymentValueValue.creatorClientID);
-        createApplication.setCorrelationId(paymentValueValue.correlationID);
+        createApplication.setApplicationName(paymentValue.creatorApplicationName);
+        createApplication.setClientId(paymentValue.creatorClientID);
+        createApplication.setCorrelationId(paymentValue.correlationID);
 
         generated.TimestampType when = new generated.TimestampType();
-        when.setYear(paymentValueValue.transactionDate.substring(0, 4));
-        when.setMonth(paymentValueValue.transactionDate.substring(4, 6));
-        when.setDay(paymentValueValue.transactionDate.substring(6, 8));
-        when.setHour(paymentValueValue.transactionTime.substring(0, 2));
-        when.setMinute(paymentValueValue.transactionTime.substring(2, 4));
-        when.setSecond(paymentValueValue.transactionTime.substring(4, 6));
+        when.setYear(paymentValue.transactionDate.substring(0, 4));
+        when.setMonth(paymentValue.transactionDate.substring(4, 6));
+        when.setDay(paymentValue.transactionDate.substring(6, 8));
+        when.setHour(paymentValue.transactionTime.substring(0, 2));
+        when.setMinute(paymentValue.transactionTime.substring(2, 4));
+        when.setSecond(paymentValue.transactionTime.substring(4, 6));
 
         generated.ActionDateType actionDate = new generated.ActionDateType();
-        actionDate.setYear(paymentValueValue.valueDate.substring(0, 4));
-        actionDate.setMonth(paymentValueValue.valueDate.substring(4, 6));
-        actionDate.setDay(paymentValueValue.valueDate.substring(6, 8));
+        actionDate.setYear(paymentValue.valueDate.substring(0, 4));
+        actionDate.setMonth(paymentValue.valueDate.substring(4, 6));
+        actionDate.setDay(paymentValue.valueDate.substring(6, 8));
 
         generated.TransactionLegType debitLeg = new generated.TransactionLegType();
-        debitLeg.setApplicationName(paymentValueValue.debitApplication);
-        debitLeg.setSortCode(paymentValueValue.debitSortCode);
-        debitLeg.setAccount(paymentValueValue.debitAccountNumber);
-        debitLeg.setName(paymentValueValue.debitAccountName);
-        debitLeg.setReference(paymentValueValue.debitPaymentReference);
-        debitLeg.setLedgerAccountNumber(paymentValueValue.debitLedgerAccountNumber);
+        debitLeg.setApplicationName(paymentValue.debitApplication);
+        debitLeg.setSortCode(paymentValue.debitSortCode);
+        debitLeg.setAccount(paymentValue.debitAccountNumber);
+        debitLeg.setName(paymentValue.debitAccountName);
+        debitLeg.setReference(paymentValue.debitPaymentReference);
+        debitLeg.setLedgerAccountNumber(paymentValue.debitLedgerAccountNumber);
 
         generated.TransactionLegType creditLeg = new generated.TransactionLegType();
-        creditLeg.setApplicationName(paymentValueValue.creditApplication);
-        creditLeg.setSortCode(paymentValueValue.creditSortCode);
-        creditLeg.setAccount(paymentValueValue.creditAccountNumber);
-        creditLeg.setName(paymentValueValue.creditAccountName);
-        creditLeg.setReference(paymentValueValue.creditPaymentReference);
-        creditLeg.setLedgerAccountNumber(paymentValueValue.creditLedgeAccountNumber);
+        creditLeg.setApplicationName(paymentValue.creditApplication);
+        creditLeg.setSortCode(paymentValue.creditSortCode);
+        creditLeg.setAccount(paymentValue.creditAccountNumber);
+        creditLeg.setName(paymentValue.creditAccountName);
+        creditLeg.setReference(paymentValue.creditPaymentReference);
+        creditLeg.setLedgerAccountNumber(paymentValue.creditLedgeAccountNumber);
 
         Payment payment = new Payment();
         payment.setCreator(createApplication);
-        payment.setReference(paymentValueValue.reference);
+        payment.setReference(paymentValue.reference);
         payment.setWhen(when);
         payment.setActionDate(actionDate);
-        payment.setCurrency(paymentValueValue.currency);
-        payment.setAmount(paymentValueValue.amount);
+        payment.setCurrency(paymentValue.currency);
+        payment.setAmount(paymentValue.amount);
         payment.setDebitLeg(debitLeg);
         payment.setCreditLeg(creditLeg);
 
